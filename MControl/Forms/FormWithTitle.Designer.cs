5da8e97ee3440aea939aa28ad55327510c4b77f8
mhf <1603291350@qq.com>
2021-01-10 15:39:24 +0800

    新功能：自定义窗口
            1、自定义窗口类添加标题栏按键高亮；
            2、窗口缩放功能；
            3、鼠标移动窗口功能。

---
+++ MControl/Forms/FormWithTitle.Designer.cs
@@ -0,0 +1,165 @@
+﻿
+namespace MControl.Forms
+{
+    partial class FormWithTitle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormWithTitle));
+            this.TitleBar = new System.Windows.Forms.Panel();
+            this.panelBut = new System.Windows.Forms.Panel();
+            this.ButClose = new System.Windows.Forms.PictureBox();
+            this.ButMaxmum = new System.Windows.Forms.PictureBox();
+            this.ButMinmum = new System.Windows.Forms.PictureBox();
+            this.Title = new System.Windows.Forms.Label();
+            this.icon = new System.Windows.Forms.PictureBox();
+            this.TitleBar.SuspendLayout();
+            this.panelBut.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ButClose)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ButMaxmum)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ButMinmum)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.icon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TitleBar
+            // 
+            this.TitleBar.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.TitleBar.Controls.Add(this.panelBut);
+            this.TitleBar.Controls.Add(this.Title);
+            this.TitleBar.Controls.Add(this.icon);
+            this.TitleBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TitleBar.Location = new System.Drawing.Point(0, 0);
+            this.TitleBar.MaximumSize = new System.Drawing.Size(0, 35);
+            this.TitleBar.MinimumSize = new System.Drawing.Size(0, 35);
+            this.TitleBar.Name = "TitleBar";
+            this.TitleBar.Size = new System.Drawing.Size(523, 35);
+            this.TitleBar.TabIndex = 0;
+            this.TitleBar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TitleBar_MouseDown);
+            this.TitleBar.MouseUp += new System.Windows.Forms.MouseEventHandler(this.TitleBar_MouseUp);
+            // 
+            // panelBut
+            // 
+            this.panelBut.Controls.Add(this.ButClose);
+            this.panelBut.Controls.Add(this.ButMaxmum);
+            this.panelBut.Controls.Add(this.ButMinmum);
+            this.panelBut.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panelBut.Location = new System.Drawing.Point(411, 0);
+            this.panelBut.MaximumSize = new System.Drawing.Size(112, 35);
+            this.panelBut.MinimumSize = new System.Drawing.Size(112, 35);
+            this.panelBut.Name = "panelBut";
+            this.panelBut.Size = new System.Drawing.Size(112, 35);
+            this.panelBut.TabIndex = 2;
+            // 
+            // ButClose
+            // 
+            this.ButClose.Image = global::MControl.Resource1.CloseBlack;
+            this.ButClose.Location = new System.Drawing.Point(80, 7);
+            this.ButClose.Name = "ButClose";
+            this.ButClose.Size = new System.Drawing.Size(20, 20);
+            this.ButClose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ButClose.TabIndex = 3;
+            this.ButClose.TabStop = false;
+            this.ButClose.MouseEnter += new System.EventHandler(this.ButClose_MouseEnter);
+            this.ButClose.MouseLeave += new System.EventHandler(this.ButClose_MouseLeave);
+            // 
+            // ButMaxmum
+            // 
+            this.ButMaxmum.Image = global::MControl.Resource1.MaximizeBlack;
+            this.ButMaxmum.Location = new System.Drawing.Point(41, 7);
+            this.ButMaxmum.Name = "ButMaxmum";
+            this.ButMaxmum.Size = new System.Drawing.Size(20, 20);
+            this.ButMaxmum.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ButMaxmum.TabIndex = 2;
+            this.ButMaxmum.TabStop = false;
+            this.ButMaxmum.MouseEnter += new System.EventHandler(this.ButMaxmum_MouseEnter);
+            this.ButMaxmum.MouseLeave += new System.EventHandler(this.ButMaxmum_MouseLeave);
+            // 
+            // ButMinmum
+            // 
+            this.ButMinmum.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ButMinmum.Image = ((System.Drawing.Image)(resources.GetObject("ButMinmum.Image")));
+            this.ButMinmum.Location = new System.Drawing.Point(3, 7);
+            this.ButMinmum.Name = "ButMinmum";
+            this.ButMinmum.Size = new System.Drawing.Size(20, 20);
+            this.ButMinmum.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ButMinmum.TabIndex = 1;
+            this.ButMinmum.TabStop = false;
+            this.ButMinmum.MouseEnter += new System.EventHandler(this.ButMinmum_MouseEnter);
+            this.ButMinmum.MouseLeave += new System.EventHandler(this.ButMinmum_MouseLeave);
+            // 
+            // Title
+            // 
+            this.Title.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.Title.AutoSize = true;
+            this.Title.Location = new System.Drawing.Point(40, 10);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(63, 14);
+            this.Title.TabIndex = 1;
+            this.Title.Text = "标题窗体";
+            // 
+            // icon
+            // 
+            this.icon.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.icon.Location = new System.Drawing.Point(3, 3);
+            this.icon.MaximumSize = new System.Drawing.Size(29, 29);
+            this.icon.MinimumSize = new System.Drawing.Size(29, 29);
+            this.icon.Name = "icon";
+            this.icon.Size = new System.Drawing.Size(29, 29);
+            this.icon.TabIndex = 0;
+            this.icon.TabStop = false;
+            // 
+            // FormWithTitle
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(523, 361);
+            this.Controls.Add(this.TitleBar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormWithTitle";
+            this.Text = "有标题的窗口";
+            this.TitleBar.ResumeLayout(false);
+            this.TitleBar.PerformLayout();
+            this.panelBut.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ButClose)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ButMaxmum)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ButMinmum)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.icon)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel TitleBar;
+        private System.Windows.Forms.PictureBox icon;
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.Panel panelBut;
+        private System.Windows.Forms.PictureBox ButMinmum;
+        private System.Windows.Forms.PictureBox ButClose;
+        private System.Windows.Forms.PictureBox ButMaxmum;
+    }
+}
---
+++ MControl/Forms/FormWithTitle.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MControl.Forms
+{
+    public partial class FormWithTitle : Form
+    {
+        public FormWithTitle()
+        {
+            InitializeComponent();
+        }
+
+        #region 按键高亮
+        /// <summary>
+        /// 鼠标进入控件后控件高亮
+        /// </summary>
+        /// <param name="p_Control"></param>
+        private void ButHighlight(Control p_Control)
+        {
+            Color color = TitleBar.BackColor;
+            p_Control.BackColor = Color.FromArgb(color.R + 20, color.G + 20, color.B + 20);
+        }
+
+        /// <summary>
+        /// 鼠标离开控件范围后还原控件颜色
+        /// </summary>
+        /// <param name="p_Control"></param>
+        private void ButRestoreColor(Control p_Control)
+        {
+            p_Control.BackColor = TitleBar.BackColor;
+        }
+
+        private void ButClose_MouseEnter(object sender, EventArgs e)
+        {
+            ButHighlight(ButClose);
+        }
+
+        private void ButClose_MouseLeave(object sender, EventArgs e)
+        {
+            ButRestoreColor(ButClose);
+        }
+
+        private void ButMaxmum_MouseEnter(object sender, EventArgs e)
+        {
+            ButHighlight(ButMaxmum);
+        }
+
+        private void ButMaxmum_MouseLeave(object sender, EventArgs e)
+        {
+            ButRestoreColor(ButMaxmum);
+        }
+
+        private void ButMinmum_MouseEnter(object sender, EventArgs e)
+        {
+            ButHighlight(ButMinmum);
+        }
+
+        private void ButMinmum_MouseLeave(object sender, EventArgs e)
+        {
+            ButRestoreColor(ButMinmum);
+        }
+
+        #endregion
+
+        #region 窗口缩放
+
+        const int WM_NCHITTEST = 0x0084;
+        const int HTLEFT = 10;      //左边界
+        const int HTRIGHT = 11;     //右边界
+        const int HTTOP = 12;       //上边界
+        const int HTTOPLEFT = 13;   //左上角
+        const int HTTOPRIGHT = 14;  //右上角
+        const int HTBOTTOM = 15;    //下边界
+        const int HTBOTTOMLEFT = 0x10;    //左下角
+        const int HTBOTTOMRIGHT = 17;     //右下角
+        protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case WM_NCHITTEST:
+                    base.WndProc(ref m);
+                    Point vPoint = new Point((int)m.LParam & 0xFFFF,
+                        (int)m.LParam >> 16 & 0xFFFF);
+                    vPoint = PointToClient(vPoint);
+                    if (vPoint.X <= 5)
+                        if (vPoint.Y <= 5)
+                            m.Result = (IntPtr)HTTOPLEFT;
+                        else if (vPoint.Y >= ClientSize.Height - 5)
+                            m.Result = (IntPtr)HTBOTTOMLEFT;
+                        else m.Result = (IntPtr)HTLEFT;
+                    else if (vPoint.X >= ClientSize.Width - 5)
+                        if (vPoint.Y <= 5)
+                            m.Result = (IntPtr)HTTOPRIGHT;
+                        else if (vPoint.Y >= ClientSize.Height - 5)
+                            m.Result = (IntPtr)HTBOTTOMRIGHT;
+                        else m.Result = (IntPtr)HTRIGHT;
+                    else if (vPoint.Y <= 5)
+                        m.Result = (IntPtr)HTTOP;
+                    else if (vPoint.Y >= ClientSize.Height - 5)
+                        m.Result = (IntPtr)HTBOTTOM;
+                    break;
+                default:
+                    base.WndProc(ref m);
+                    break;
+            }
+        }
+        #endregion
+
+        #region 窗口移动
+        private Point m_PointMouse;                   //保存鼠标移动前的坐标
+        /// <summary>
+        /// 鼠标在标题栏按下后开始监听鼠标移动事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TitleBar_MouseMove(object sender, MouseEventArgs e)
+        {
+            Point l_Point = this.Location;
+            this.Location = new Point(l_Point.X + (MousePosition.X - m_PointMouse.X), l_Point.Y + (MousePosition.Y - m_PointMouse.Y));
+            m_PointMouse = MousePosition;
+        }
+
+        /// <summary>
+        /// 鼠标移动事件，获取鼠标当前坐标
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TitleBar_MouseDown(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left)
+            {
+                m_PointMouse = MousePosition;
+                this.TitleBar.MouseMove += new System.Windows.Forms.MouseEventHandler(this.TitleBar_MouseMove);
+            }
+        }
+
+        /// <summary>
+        /// 鼠标按键释放后停止监听鼠标坐标
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TitleBar_MouseUp(object sender, MouseEventArgs e)
+        {
+            this.TitleBar.MouseMove -= new System.Windows.Forms.MouseEventHandler(this.TitleBar_MouseMove);
+        }
+
+        #endregion
+    }
+}
